b963d4cadfedef521ef71380a3e9a9a83c7e3ac5
Sailendra <DS@MBP.local>
2021-05-12 16:55:34 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7fb5cda1-87ce-421f-9fa8-14935e436b2d';
+export const MODEL_CLOSE_SEC = 2;
